123a7c96a8eb78a8ef0af55d1bdaaf2a7292be0a
PauPau1003 <dominicj.2022@scis.smu.edu.sg>
2023-10-18 19:30:24 +0800

    Added vue framework in the folder testing-wad

---
+++ testing-wad/src/components/Categories.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="categories">
+      <ul class="m-4 nav nav-underline">
+        <li class="nav-item">
+          <a class="nav-link text-dark" aria-current="page" href="#">Graphic & Design</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#">Programming & Tech</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-dark" href="#">Digital Marketing</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Video Animation</a>
+        </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Writing and Translation</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Music and Audio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Business</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Data</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Photography</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">AI service</a>
+          </li>
+      </ul>
+    </div>
+</template>
+<script>
+
+export default {
+  name: 'Categories',
+};
+</script>
+<style scoped>
+
+</style>
