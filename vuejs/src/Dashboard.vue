19926f240d7dbe52796b81031b16c57b292db57b
zanzzxc <gxlee.2022@scis.smu.edu.sg>
2023-10-29 17:05:28 +0800

    vue

---
+++ vuejs/src/Dashboard.vue
@@ -0,0 +1,121 @@
+<script>
+anychart.onDocumentReady(function () {  
+									// add the data
+									var data = anychart.data.set([
+									["Referral", 1],
+									["Direct", 2],
+									["Organic Search", 7]
+									]);
+									// create a pie chart instance with the passed data
+									var chart = anychart
+									.pie(data)
+									// set the inner radius to make a donut chart
+									.innerRadius("55%");
+									// set the container id for the chart
+									chart.container("container");
+									// draw the resulting chart
+									chart.draw();
+								});
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/js/bootstrap.js'
+
+</script>
+<template>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="description" content="" />
+	<meta name="keywords" content="bootstrap, bootstrap5" />
+
+
+	<title>Hello this is wad</title>
+</head>
+<body>
+
+		<div class="container-fluid" style="background-color:lightgray ;border-radius: 5px;">
+			<div class="row mb-3 align-items-center">
+				<div class="col-md-6 col-12">
+					<h2 class="font-weight-bold text-primary heading mt-5">Your Executive Dashboard</h2>
+				</div>
+				<!-- <div class="col-lg-6 text-lg-end">
+					<p><a href="#" target="_blank" class="btn btn-primary text-white py-3 px-4">View all properties</a></p>
+				</div> -->
+				</div>
+				<div class="row mb-3">
+					<div class="col-md-3 col-12 mx-3" data-aos="fade-up" data-aos-delay="300" style="height:80%;border: 2px solid white; border-radius:5px;background-color: white; ">
+						<p><small class="text-muted">Auto date range</small><br><img src="images/calendar.png" style="width:20px ; height:20px;"> This Month</p>
+					</div>
+					<div class="col-md-2 col-12 mx-3" data-aos="fade-up" data-aos-delay="300" style="height:90%;border: 2px solid white; border-radius:5px;background-color: white; ">
+						<p><small class="text-muted">Products</small><br>All</p>
+					</div>
+					<div class="col-md-2 col-12 mx-3" data-aos="fade-up" data-aos-delay="300" style="height:90%;border: 2px solid white; border-radius:5px;background-color: white; ">
+						<p><small class="text-muted">Posts</small><br>All</p>
+					</div>
+				</div>
+				<div class="row mb-3">
+					<div class="col-md-3 col-6 mx-3"  data-aos="fade-up" data-aos-delay="300" style="border: 2px solid white; border-radius:5px;background-color: white; ">
+						<h3 class="mb-3">New Sales</h3>
+						<div style="text-align: center;">
+						<h1>230</h1>
+						<h2 style="color:green; font-weight:200;">25%</h2>
+						<p style="font-weight:100;">vs previous month</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-6 mx-2"  data-aos="fade-up" data-aos-delay="500" style="border: 2px solid white; border-radius:5px;background-color: white; ">
+							<h3 class="mb-3">Business Growth Rate</h3>
+							<div style="text-align: center;">
+								<h1>17.23%</h1>
+								<h2 style="color:green; font-weight:200;">25%</h2>
+								<p style="font-weight:100;">vs previous month</p>
+								</div>
+					</div>
+					<div class="col-md-3 col-6 mx-2"  data-aos="fade-up" data-aos-delay="400" style="border: 2px solid white; border-radius:5px;background-color: white; ">
+							<h3 class="mb-3">MRR</h3>
+							<div style="text-align: center;">
+								<h1>$4,596</h1>
+								<h2 style="color:green; font-weight:200;">7.8%</h2>
+								<p style="font-weight:100;">vs previous month</p>
+								</div>
+					</div>
+					<div class="col-md-2 col-6 mx-2"  data-aos="fade-up" data-aos-delay="600" style="border: 2px solid white; border-radius:5px;background-color: white; ">
+							<h3 class="mb-3">Webpage Views</h3>
+							<div id="container"></div>
+
+					</div>	
+				</div>
+				<div class="row mb-3">
+					<div class="col-md-6 col-12 mx-3"  data-aos="fade-up" data-aos-delay="400" style="border: 2px solid white; border-radius:5px;background-color: white; ">
+						<h3 class="mb-3">Business Progress</h3>
+						<img src="images/success.png" style="height: 400px;width: 700px;">
+					</div>
+					<div class="col-md-5 col-12"  data-aos="fade-up" data-aos-delay="400" style="margin-left:40px;border: 2px solid white; border-radius:5px;background-color: white; ">
+						<h3 class="mb-3">MRR</h3>
+						<img src="images/MRRexample.png" style="height: 400px;width: 560px;">
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+
+
+
+  </body>
+  </html>
+
+
+
+</template>
---
+++ vuejs/src/components/spnavbar.vue
@@ -0,0 +1,137 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top-navbar">
+    <div class="container">
+      <!-- Logo -->
+      <a class="navbar-brand"><img src="/BookMeLogo-removebg-preview.png" alt="Logo" style="height: 40px"></a>
+      <!-- Toggle Button -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
+      >
+        <span class="navbar-toggler-icon">
+          <i class="fa fa-bars" aria-hidden="true" style="color: #1c1c1c"></i>
+        </span>
+      </button>
+      <!-- Sidebar -->
+      <div
+        class="sidebar offcanvas offcanvas-start"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <!-- Sidebar Header -->
+        <div class="offcanvas-header text-white border-white">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src="../../public/BookMeLogo.png" style="height: 40px;"></h5>
+          <button
+            type="button"
+            class="btn-close btn-close-white shadow-none"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <!-- Sidebar Body -->
+        <div class="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
+          <ul class="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
+            <li class="nav-item mx-2">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item mx-2">
+              <a class="nav-link" href="#about">Dashboard</a>
+            </li>
+            <li class="nav-item mx-2">
+              <a class="nav-link" href="#about">Learning Avenue</a>
+            </li>
+            <li class="nav-item mx-2">
+              <a class="nav-link" href="#about">Permits/Requirements</a>
+            </li>
+            <li class="nav-item mx-2">
+              <a class="nav-link" href="#about" style="color:green;margin-left:600px">Switch to Buying</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/js/bootstrap.js'
+
+export default {
+  name: 'navbar',
+};
+</script>
+
+<style>
+
+body{
+
+background-blend-mode: multiply;
+background-position: center;
+background-size: cover;
+background-repeat: no-repeat;
+margin: 0;
+}
+
+@media(max-width:991px){
+.sidebar.offcanvas{
+    background-color:rgba(255,255,255,0.15);
+    backdrop-filter: blur(10px);
+}
+} 
+
+.navbar{
+margin-top: 20px;
+}
+.signupBut {
+background-color: rgb(244, 241, 241);
+border: 1px ;
+border-color: green;
+color: rgb(56, 56, 56);
+}
+.form-search.form-control {
+width:300px
+}
+
+.maincontent{
+background: url('images/bg.jpg') rgba(0,0,0,0.3);
+}
+
+.sticky-top-navbar {
+position: sticky;
+top: 0; /* Stick to the top of the viewport */
+z-index: 100; /* Adjust the z-index as needed */
+}
+
+.navbar-dark .navbar-nav .nav-link {
+color: black !important;
+}
+
+/* Target specifically the "Home" link */
+.navbar-dark .navbar-nav .nav-item.active .nav-link {
+color: black !important;
+}
+
+
+.navbar-toggler-icon {
+
+border-color: black;
+}
+
+.navbar .text-white {
+color: black; /* Change the text color to black */
+}
+
+</style>
